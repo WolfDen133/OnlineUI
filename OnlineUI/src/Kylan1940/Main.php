8410c66a8f61fe04201e189013a8e452f3c6ba2f
Kylan1940 <70039339+Kylan1940@users.noreply.github.com>
2021-02-23 09:20:24 +0000

    Change public function

--- OnlineUI/src/Kylan1940/Main.php
+++ OnlineUI/src/Kylan1940/Main.php
@@ -52,3 +52,94 @@ class Main extends PluginBase implements Listener{
 		return $form;
 	}
 }
+<?php
+
+namespace Kylan1940;
+
+use pocketmine\Server;
+
+use pocketmine\Player;
+
+use pocketmine\plugin\Plugin;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\Listener;
+
+use pocketmine\command\Command;
+
+use pocketmine\command\CommandSender;
+
+use jojoe77777\FormAPI;
+
+use jojoe77777\FormAPI\SimpleForm;
+
+class Main extends PluginBase implements Listener{
+
+	
+
+	public function onEnable(){
+
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+		$this->getLogger()->info("Plugin Enable");
+
+	}
+
+	
+
+	public function onCommand(CommandSender $player, Command $cmd, string $label, array $data): bool{
+
+		switch($cmd->getName()){
+
+		case "online":
+
+		  $result = $data;
+
+			if($player instanceof Player){
+
+				$this->OnlineUI($player);
+
+				return true;
+
+			}
+
+		}
+
+		return true;
+
+	}
+
+	public function OnlineUI($player){
+
+	  $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+
+    $form = $api->createSimpleForm (function (Player $player, int $data = null) {
+
+        $result = $data;
+
+        if($result === null){
+
+            return true;
+
+			}
+
+		});
+
+		$form->setTitle("§e-=Online §aUI=-");
+
+		$form->setContent("Players:");
+
+		foreach($this->getServer()->getOnlinePlayers() as $online){
+
+			$form->addButton($online->getName(), -1, "", $online->getName(), 0, "textures/ui/confirm");
+
+		}
+
+		$form->sendToPlayer($p);
+
+		return $form;
+
+	}
+
+}
